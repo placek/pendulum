2c88c62ee70b1e89bd8968efd340a0c19112800b
Paweł Placzyński <placek@ragnarson.com>
2011-10-12 00:43:21 +0200

    [#9d3c7c] added a Tk window running a simulation
     * for now only two buttons: simulation and quit;

--- pendulum.py
+++ pendulum.py
@@ -4,7 +4,28 @@
 #
 # by placek@ragnarson.com
 #
-
+import sys
 from lib.simulation import SimulationRunner
+from Tkinter import *
 
-SimulationRunner().start()
+class PendulumApp:
+
+  def __init__(self, master):
+    frame = Frame(master)
+    frame.pack()
+    # run_sumulation_button
+    self.run_simulation_button = Button(frame, text = "Simulation", command = self.run_simulation)
+    self.run_simulation_button.pack(side = LEFT)
+    # quit_button
+    self.quit_button = Button(frame, text = "Quit", command = self.quit)
+    self.quit_button.pack(side=LEFT)
+
+  def quit(self):
+    sys.exit()
+
+  def run_simulation(self):
+    SimulationRunner().start()
+
+root = Tk()
+app = PendulumApp(root)
+root.mainloop()
